287b0aa6ffaa38b187eb293ae53670a89ca43d22
Joao Lage <my@mail.com>
2018-10-18 23:45:36 +0100

    starting chrome in headless mode

--- main.py
+++ main.py
@@ -8,7 +8,14 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 
 
-driver = webdriver.Chrome()
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--window-size=1420,1080')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+
+
+driver = webdriver.Chrome(chrome_options=chrome_options)
 server = smtplib.SMTP('smtp.gmail.com', 587) 
 
 target = 'https://tickets.wbstudiotour.co.uk/webstore/shop/ViewItems.aspx?CG=HPTST2&C=TIX2'
